e7ac8e4048d2f7d0f6dce10a1481cb55266afd12
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2022-09-09 21:04:23 +0200

    [doc] Modify the shell file more comprehensive

--- run_experiment.sh
+++ run_experiment.sh
@@ -8,40 +8,40 @@ done
 end=$((${start}+${dx}))
 
 run_bench () {
-    bench_name=${1}
-    dataset_name=${2}
-    for seed in `seq $start $end`
+    seed=${1}
+    bench_name=${2}
+    dataset_name=${3}
+    for opt_name in rgpe-parego rgpe-ehvi tstr-parego tstr-ehvi tpe
     do
-        for opt_name in tpe rgpe-parego rgpe-ehvi tstr-parego tstr-ehvi
-        do
-            prefix="python run.py --exp_id ${seed} --opt_name ${opt_name} --bench_name ${bench_name} --dataset_name ${dataset_name}"
-            if [[ "$opt_name" == "tpe" ]]
-            then
-                cmd="${prefix} --warmstart True --uniform_transform True"
-                echo $cmd
-                $cmd
-                echo `date '+%y/%m/%d %H:%M:%S'`
-
-                cmd="${prefix} --warmstart False"
-                echo $cmd
-                $cmd
-                echo `date '+%y/%m/%d %H:%M:%S'`
-            fi
-
-            cmd="${prefix} --warmstart True"
+        prefix="python run.py --exp_id ${seed} --opt_name ${opt_name} --bench_name ${bench_name} --dataset_name ${dataset_name}"
+        if [[ "$opt_name" == "tpe" ]]
+        then
+            cmd="${prefix} --warmstart True --uniform_transform True"
             echo $cmd
             $cmd
             echo `date '+%y/%m/%d %H:%M:%S'`
-        done
+
+            cmd="${prefix} --warmstart False"
+            echo $cmd
+            $cmd
+            echo `date '+%y/%m/%d %H:%M:%S'`
+        fi
+        cmd="${prefix} --warmstart True"
+        echo $cmd
+        $cmd
+        echo `date '+%y/%m/%d %H:%M:%S'`
     done
 }
 
-for dataset in so_en sw_en tl_en
+for seed in `seq $start $end`
 do
-    run_bench "nmt" "${dataset}"
-done
+    for dataset in so_en sw_en tl_en
+    do
+        run_bench ${seed} "nmt" "${dataset}"
+    done
 
-for dataset in slice_localization protein_structure naval_propulsion parkinsons_telemonitoring
-do
-    run_bench "hpolib" "${dataset}"
+    for dataset in slice_localization protein_structure naval_propulsion parkinsons_telemonitoring
+    do
+        run_bench ${seed} "hpolib" "${dataset}"
+    done
 done
