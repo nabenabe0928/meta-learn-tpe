3ae94e152e87846d70d263c3967c7145bbabd381
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2022-09-26 02:44:56 +0900

    [doc] Add visualizations from experiments

--- viz/constants.py
+++ viz/constants.py
@@ -3,33 +3,73 @@ N_INIT = N_SAMPLES * 5 // 100
 N_RUNS = 20
 N_OBJ = 2
 COSTS_SHAPE = (N_RUNS, N_SAMPLES, N_OBJ)
-LEVELS = [N_RUNS // 4, N_RUNS // 2, (3 * N_RUNS) // 4]
-# LEVELS = [N_RUNS // 2] * 3
-HV_MODE = True
+# LEVELS = [N_RUNS // 4, N_RUNS // 2, (3 * N_RUNS) // 4]
+LEVELS = [N_RUNS // 2] * 3
 Q, DF = [0.10, 0.15][0], [1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0][2]
 WARMSTART_OPT = f"tpe_q={Q:.2f}_df={DF:.1f}"
 COLOR_LABEL_DICT = {
     "random": ("olive", "Random"),
-    # f"naive_metalearn_tpe_q={Q:.2f}": ("green", "Uniform weight"),
-    f"normal_tpe_q={Q:.2f}": ("blue", "MOTPE"),
+    "warmstart_config": ("black", "Warmstart configs"),
     # f"tpe_q={Q:.2f}_df={DF:.1f}": ("red", f"Meta-learn TPE df={DF:.1f}"),
     f"tpe_q={Q:.2f}_df={DF:.1f}": ("red", "Meta-learn TPE"),
-    "warmstart_config": ("black", "Warmstart configs"),
-    "tstr-ehvi": ("cyan", "TST-R EHVI"),
-    "tstr-parego": ("magenta", "TST-R ParEGO"),
-    "rgpe-ehvi": ("purple", "RGPE EHVI"),
-    "rgpe-parego": ("brown", "RGPE ParEGO"),
+    # f"naive_metalearn_tpe_q={Q:.2f}": ("green", "Uniform weight"),
+    f"normal_tpe_q={Q:.2f}": ("blue", "MOTPE"),
+    "tstr-ehvi": ("violet", "TST-R EHVI"),
+    "tstr-parego": ("violet", "TST-R ParEGO"),
+    "rgpe-ehvi": ("lime", "RGPE EHVI"),
+    "rgpe-parego": ("lime", "RGPE ParEGO"),
+}
+LINESTYLES_DICT = {
+    "random": "dashdot",
+    "warmstart_config": "dashdot",
+    # f"naive_metalearn_tpe_q={Q:.2f}": "o",
+    f"normal_tpe_q={Q:.2f}": "solid",
+    f"tpe_q={Q:.2f}_df={DF:.1f}": "solid",
+    "tstr-ehvi": "dotted",
+    "tstr-parego": "dashed",
+    "rgpe-ehvi": "dotted",
+    "rgpe-parego": "dashed"
 }
 MARKER_DICT = {
     "random": "",
     # f"naive_metalearn_tpe_q={Q:.2f}": "o",
-    f"normal_tpe_q={Q:.2f}": "o",
+    f"normal_tpe_q={Q:.2f}": "*",
     f"tpe_q={Q:.2f}_df={DF:.1f}": "*",
     "warmstart_config": "",
-    "tstr-ehvi": "v",
-    "tstr-parego": "^",
-    "rgpe-ehvi": "1",
-    "rgpe-parego": "2"
+    "tstr-ehvi": "s",
+    "tstr-parego": "o",
+    "rgpe-ehvi": "s",
+    "rgpe-parego": "o"
+}
+EAF_ZOOM_DICT = {
+    "naval_propulsion": 2.5,
+    "parkinsons_telemonitoring": 3,
+    "protein_structure": 3,
+    "slice_localization": 3,
+}
+EAF_ANCHOR_DICT = {
+    "naval_propulsion": (0.995, 1.038),
+    "parkinsons_telemonitoring": (1.08, 0.998),
+    "protein_structure": (0.995, 1.08),
+    "slice_localization": (0.995, 1.13),
+}
+EAF_ASPECT_DICT = {
+    "naval_propulsion": 50,
+    "parkinsons_telemonitoring": 100,
+    "protein_structure": 600,
+    "slice_localization": 400,
+}
+EAF_INSET_XLIM_DICT = {
+    "naval_propulsion": (0, 160),
+    "parkinsons_telemonitoring": (0, 100),
+    "protein_structure": (40, 200),
+    "slice_localization": (0, 1000),
+}
+EAF_INSET_YLIM_DICT = {
+    "naval_propulsion": (2e-5, 1e-3),
+    "parkinsons_telemonitoring": (8e-3, 2e-2),
+    "protein_structure": (2.2e-1, 3e-1),
+    "slice_localization": (1e-4, 2e-3),
 }
 BENCH_NAMES = ["hpolib", "nmt", "hpobench"]
 DATASET_NAMES = {
@@ -68,6 +108,12 @@ OBJ_NAMES_DICT = {
     "hpolib": ["runtime", "valid_mse"],
     "nmt": ["decoding_time", "bleu"],
     "hpobench": ["precision", "bal_acc"]
+}
+OBJ_LABEL_DICT = {
+    "runtime": "Runtime",
+    "valid_mse": "Validation MSE",
+    "decoding_time": "Runtime",
+    "bleu": "BLEU",
 }
 LARGER_IS_BETTER_DICT = {
     "hpolib": None,
--- viz/viz.py
+++ viz/viz.py
@@ -17,13 +17,19 @@ from constants import (
     COLOR_LABEL_DICT,
     COSTS_SHAPE,
     DATASET_NAMES,
-    HV_MODE,
+    EAF_ANCHOR_DICT,
+    EAF_ASPECT_DICT,
+    EAF_INSET_XLIM_DICT,
+    EAF_INSET_YLIM_DICT,
+    EAF_ZOOM_DICT,
     LARGER_IS_BETTER_DICT,
     LEVELS,
+    LINESTYLES_DICT,
     LOGSCALE_DICT,
     MARKER_DICT,
     N_SAMPLES,
     NAME_DICT,
+    OBJ_LABEL_DICT,
     OBJ_NAMES_DICT,
     TICK_PARAMS,
 )
@@ -46,6 +52,39 @@ def disable_axis_label(
         ax.set_ylabel("")
 
 
+def add_inset_axis_for_eaf(
+    ax: plt.Axes,
+    dataset_name: str,
+    eaf_plot: EmpiricalAttainmentFuncPlot,
+    surfs_list: List[np.ndarray],
+    **plot_kwargs,
+) -> None:
+
+    axins = zoomed_inset_axes(
+        ax,
+        zoom=EAF_ZOOM_DICT[dataset_name],
+        bbox_to_anchor=EAF_ANCHOR_DICT[dataset_name],
+        bbox_transform=ax.transAxes,
+        loc="upper right",
+        borderpad=0.1,
+        axes_kwargs=dict(aspect=EAF_ASPECT_DICT[dataset_name]),
+    )
+
+    eaf_plot.plot_multiple_surface_with_band(axins, surfs_list=surfs_list, **plot_kwargs)
+    label = "True Pareto front"
+    eaf_plot.plot_true_pareto_surface(axins, color="black", label=label, linestyle="--", marker="*")
+
+    axins.set_xlim(EAF_INSET_XLIM_DICT[dataset_name])
+    axins.set_ylim(EAF_INSET_YLIM_DICT[dataset_name])
+    axins.grid(which="minor", color="gray", linestyle=":")
+    axins.grid(which="major", color="black")
+    axins.tick_params(**TICK_PARAMS)
+    axins.tick_params(axis='y', which='major', labelsize=1)
+    axins.tick_params(axis='y', which='minor', labelsize=1)
+    disable_axis_label(axins)
+    mark_inset(ax, axins, loc1=2, loc2=4, fc="gray", ec="red", alpha=0.2)
+
+
 def plot_eaf(
     ax: plt.Axes,
     eaf_plot: EmpiricalAttainmentFuncPlot,
@@ -55,28 +94,36 @@ def plot_eaf(
     set_ylabel: bool,
     **kwargs
 ) -> Tuple[List[Any], List[str]]:
-    surfs_list, colors, labels = [], [], []
+    surfs_list, colors, labels, markers, linestyles = [], [], [], [], []
     for opt_name, color_label in COLOR_LABEL_DICT.items():
         color, label = color_label
         colors.append(color)
         labels.append(label)
+        linestyles.append(LINESTYLES_DICT[opt_name])
+        markers.append(MARKER_DICT[opt_name])
         costs = get_costs(obj_names, dataset_name, opt_name)
         surfs = get_empirical_attainment_surface(costs.copy(), levels=LEVELS, **kwargs)
         surfs_list.append(surfs)
     else:
-        lines = eaf_plot.plot_multiple_surface_with_band(ax, surfs_list=surfs_list, colors=colors, labels=labels)
+        plot_kwargs = dict(
+            colors=colors, labels=labels, linestyles=linestyles, markers=markers, markersize=3,
+        )
+        lines = eaf_plot.plot_multiple_surface_with_band(ax, surfs_list=surfs_list, **plot_kwargs)
         label = "True Pareto front"
         lines.append(eaf_plot.plot_true_pareto_surface(ax, color="black", label=label, linestyle="--", marker="*"))
         labels.append(label)
 
+        if not dataset_name.endswith("_en"):
+            add_inset_axis_for_eaf(ax, dataset_name, eaf_plot, surfs_list, **plot_kwargs)
+
     if set_xlabel:
-        ax.set_xlabel(obj_names[0])
+        ax.set_xlabel(OBJ_LABEL_DICT[obj_names[0]])
     if set_ylabel:
-        ax.set_ylabel(obj_names[1])
+        ax.set_ylabel(OBJ_LABEL_DICT[obj_names[1]])
     return lines, labels
 
 
-def add_inset_axis(
+def add_inset_axis_for_hv(
     ax: plt.Axes,
     lines: List[Any],
     eaf_plot: EmpiricalAttainmentFuncPlot,
@@ -118,16 +165,17 @@ def plot_hv(
 
     color_label_dict = {k: v for k, v in COLOR_LABEL_DICT.items() if k != "warmstart_config"}
     n_opts = len(color_label_dict)
-    costs_array, colors, labels, markers = np.empty((n_opts, *COSTS_SHAPE)), [], [], []
+    costs_array, colors, labels, markers, linestyles = np.empty((n_opts, *COSTS_SHAPE)), [], [], [], []
     for idx, (opt_name, color_label) in enumerate(color_label_dict.items()):
         color, label = color_label
         colors.append(color)
         labels.append(label)
         markers.append(MARKER_DICT[opt_name])
+        linestyles.append(LINESTYLES_DICT[opt_name])
         costs_array[idx] = get_costs(obj_names, dataset_name, opt_name)
     else:
         label = "True Pareto front"
-        plot_kwargs = dict(colors=colors, labels=labels, markers=markers, markevery=5)
+        plot_kwargs = dict(colors=colors, labels=labels, markers=markers, markevery=5, linestyles=linestyles)
         lines = eaf_plot.plot_multiple_hypervolume2d_with_band(ax, costs_array, log=log, **plot_kwargs)
         lines.append(
             eaf_plot.plot_true_pareto_surface_hypervolume2d(
@@ -138,7 +186,7 @@ def plot_hv(
         ax.set_ylim(ymin=0.78, ymax=1.01)
 
         if not dataset_name.endswith("en"):
-            add_inset_axis(ax, lines, eaf_plot, costs_array, log, **plot_kwargs)
+            add_inset_axis_for_hv(ax, lines, eaf_plot, costs_array, log, **plot_kwargs)
 
         disable_axis_label(ax, set_xlabel=set_xlabel, set_ylabel=set_ylabel)
         return lines, labels
@@ -175,7 +223,7 @@ def plot(
     return lines, labels
 
 
-def plot_for_hpolib(subplots_kwargs, legend_kwargs) -> None:
+def plot_hv_for_hpolib(subplots_kwargs, legend_kwargs, hv_mode: bool) -> None:
     _, axes = plt.subplots(**subplots_kwargs)
     for data_id in range(4):
         r, c = data_id // 2, data_id % 2
@@ -186,16 +234,19 @@ def plot_for_hpolib(subplots_kwargs, legend_kwargs) -> None:
             set_ylabel=set_ylabel,
             bench_id=0,
             data_id=data_id,
-            hv_mode=HV_MODE,
+            hv_mode=hv_mode,
         )
         lines, labels = plot(axes[r][c], **kwargs)
     else:
         axes[-1][0].legend(handles=lines, labels=labels, ncol=len(labels), **legend_kwargs)
 
-    plt.savefig("figs/hv2d-hpolib.pdf", bbox_inches='tight')
+    if hv_mode:
+        plt.savefig("figs/hv2d-hpolib.pdf", bbox_inches='tight')
+    else:
+        plt.savefig("figs/eaf-hpolib.pdf", bbox_inches='tight')
 
 
-def plot_for_nmt(subplots_kwargs, legend_kwargs) -> None:
+def plot_hv_for_nmt(subplots_kwargs, legend_kwargs, hv_mode: bool) -> None:
     _, axes = plt.subplots(**subplots_kwargs)
     for data_id in range(3):
         set_ylabel = data_id == 0
@@ -204,16 +255,20 @@ def plot_for_nmt(subplots_kwargs, legend_kwargs) -> None:
             set_ylabel=set_ylabel,
             bench_id=1,
             data_id=data_id,
-            hv_mode=HV_MODE,
+            hv_mode=hv_mode,
         )
         lines, labels = plot(axes[data_id], **kwargs)
     else:
         axes[1].legend(handles=lines, labels=labels, ncol=len(labels), **legend_kwargs)
 
-    plt.savefig("figs/hv2d-nmt.pdf", bbox_inches='tight')
+    if hv_mode:
+        plt.savefig("figs/hv2d-nmt.pdf", bbox_inches='tight')
+    else:
+        plt.savefig("figs/eaf-nmt.pdf", bbox_inches='tight')
 
 
 if __name__ == "__main__":
+    HV_MODE = False
     os.makedirs("figs/", exist_ok=True)
     subplots_kwargs = dict(
         nrows=2,
@@ -222,20 +277,20 @@ if __name__ == "__main__":
         sharey=HV_MODE,
         figsize=(20, 10),
         gridspec_kw=dict(
-            wspace=0.05,
-            hspace=0.125,
+            wspace=0.05 if HV_MODE else 0.08,
+            hspace=0.125 if HV_MODE else 0.17,
         )
     )
     legend_kwargs = dict(
         loc='upper center',
-        fontsize=14,
-        bbox_to_anchor=(1, -0.15),
+        fontsize=14 if HV_MODE else 12,
+        bbox_to_anchor=(0.98, -0.15),
         fancybox=False,
         shadow=False,
     )
-    plot_for_hpolib(subplots_kwargs, legend_kwargs)
+    # plot_hv_for_hpolib(subplots_kwargs, legend_kwargs, hv_mode=HV_MODE)
 
     subplots_kwargs.pop("nrows")
     subplots_kwargs.update(ncols=3, figsize=(30, 5))
-    legend_kwargs.update(bbox_to_anchor=(0.5, -0.15))
-    # plot_for_nmt(subplots_kwargs, legend_kwargs)
+    legend_kwargs.update(bbox_to_anchor=(0.5, -0.12), fontsize=18)
+    plot_hv_for_nmt(subplots_kwargs, legend_kwargs, hv_mode=HV_MODE)
